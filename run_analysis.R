a3e62abdd354a6001e14799daa4de0a40a0b90de
PaulCoursera <pee_wee9@hotmail.com>
2014-11-23 18:37:06 +0100

    fixing bugs

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,19 @@
-#setwd("/tmp/Tidy-005")
+# Step0. Prepare + download files and libraries
 
-## check required packages. install when not found.
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+datafile <- "getdata-projectfiles-UCI.zip"
+datadir  <- "./UCI HAR Dataset"
+
+##
+# check required library-packages. install when not found.
+##
 if (!require("data.table")) 
 {
   install.packages("data.table")
   require("data.table")
 }
 
-if (!require("reshape2")) 
-{
-  install.packages("reshape2")
-  require("reshape2")
-}
-
 library(data.table)
-library(reshape2)
-
-
-## declarations
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-datafile <- "getdata-projectfiles-UCI.zip"
-datadir  <- "./UCI HAR Dataset"
-testdir  <- paste(datadir, "test", sep = "/")
-traindir <- paste(datadir, "train", sep = "/")
-
-###
-# download and unzip the data in the dir called UCI_HAR_Dataset
-###
 
 ## download file if not found
 if (!file.exists(datafile))
@@ -43,124 +30,86 @@ if (!file.exists(datadir))
   message("Finhed unzipping data...")
 }
 
-# read train files
-dtSubjectTrain <- fread('./UCI HAR Dataset/train/subject_train.txt')
-dtSubjectTest <- fread('./UCI HAR Dataset/test/subject_test.txt')
 
-# read activity files
-dtActivityTrain <- fread('./UCI HAR Dataset/train/y_train.txt')
-dtActivityTest <- fread('./UCI HAR Dataset/test/y_test.txt')
+##
+# Step1. Merges the training and the test sets to create one data set.
+##
+setwd("C:/tmp/Week3")
 
-# read data files
-dtTrain <- data.table(read.table('./UCI HAR Dataset/train/X_train.txt'))
-dtTest <- data.table(read.table('./UCI HAR Dataset/test/X_test.txt'))
+tmp1 <- read.table("./UCI HAR Dataset/train/X_train.txt")
+tmp2 <- read.table("./UCI HAR Dataset/test/X_test.txt")
+X <- rbind(tmp1, tmp2)
 
-###
-# 1. merge data tables
-###
-dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-setnames(dtSubject, "V1", "subject")
-dtActivity <- rbind(dtActivityTrain, dtActivityTest)
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
+tmp1 <- read.table("./UCI HAR Dataset/train/y_train.txt")
+tmp2 <- read.table("./UCI HAR Dataset/test/y_test.txt") 
+Y <- rbind(tmp1, tmp2)
 
-# merge columns
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
+tmp1 <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+tmp2 <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+S <- rbind(tmp1, tmp2)
 
-# create key on the datatable
-setkey(dt, subject, activityNum)
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 
-###
-# 2. Extract only the mean and standard deviation
-###
-dtFeatures <- fread('./UCI HAR Dataset/features.txt')
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
+##
+# Step2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+##
+columns_to_keep <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+X <- X[, columns_to_keep]
 
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
+names(X) <- gsub("\\(\\)", "", features[columns_to_keep, 2]) # remove "()"
+names(X) <- gsub("mean", "Mean", names(X)) 
+names(X) <- gsub("std", "Std", names(X)) 
+names(X) <- gsub("-", "", names(X)) # remove "-"   
 
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with = FALSE]
+##
+# Step3. Uses descriptive activity names to name the activities in the data set
+##
+activities[, 2] <- tolower(gsub("_", "", activities[, 2]))
+substr(activities[2, 2], 8, 8) <- toupper(substr(activities[2, 2], 8, 8))
+substr(activities[3, 2], 8, 8) <- toupper(substr(activities[3, 2], 8, 8))
+activityLabel <- activities[Y[, 1], 2]
+Y[, 1] <- activityLabel
+names(Y) <- "activity"
 
-###
-# 3. Use descriptive activity names
-###
-dtActivityNames <- fread('./UCI HAR Dataset/activity_labels.txt')
-setnames(dtActivityNames, 
-         names(dtActivityNames), 
-         c("activityNum", "activityName"))
+##
+# Step4. Appropriately labels the data set with descriptive activity  names. 
+##
+names(S) <- "subject"
+cleaned <- cbind(S, Y, X)
 
-###
-# 4. Label with descriptive activity names
-###
-dt <- merge(dt, 
-            dtActivityNames, 
-            by = "activityNum", 
-            all.x = TRUE)
-setkey(dt, subject, activityNum, activityName)
-dt <- data.table(melt(dt, 
-                      key(dt), 
-                      variable.name = "featureCode"))
-dt <- merge(dt, 
-            dtFeatures[, list(featureNum, featureCode, featureName)], 
-            by = "featureCode", 
-            all.x = TRUE)
+# write out the dataset1
+write.table(cleaned, "merged_data.txt") 
 
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
+##
+# Step5. Creates a second, independent tidy data set with the average of 
+# each variable for each activity and each subject. 
+##
 
-#helper for regular expression
-grepthis <- function(regex) {grepl(regex, dt$feature)}
+uniqueSubjects <- unique(S)[, 1]
+subjectCount <- length(unique(S)[, 1])
+activityCount <- length(activities[, 1])
+columnCount <- dim(cleaned)[2]
 
-n <- 2
-y <- matrix(seq(1, n), 
-            nrow = n)
-x <- matrix(c(grepthis("^t"), 
-              grepthis("^f")), 
-            ncol = nrow(y))
-dt$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
-dt$featInstrument <- factor(x %*% y, 
-                            labels = c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), 
-              grepthis("GravityAcc")), 
-            ncol = nrow(y))
-dt$featAcceleration <- factor(x %*% y, 
-                              labels = c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), 
-              grepthis("std()")), 
-            ncol = nrow(y))
-dt$featVariable <- factor(x %*% y, 
-                          labels = c("Mean", "SD"))
+result <- matrix(NA, nrow = subjectCount * activityCount, ncol = columnCount) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(cleaned)
 
-dt$featJerk <- factor(grepthis("Jerk"), 
-                      labels = c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), 
-                           labels = c(NA, "Magnitude"))
+#copy the values into the new array
+row <- 1
+for (subject in 1:subjectCount) 
+{
+  for (activity in 1:activityCount) 
+  {
+    result[row, 1] <- uniqueSubjects[subject]
+    result[row, 2] <- activities[activity, 2]
+    tmp <- cleaned[cleaned$subject == subject & cleaned$activity == activities[activity, 2], ]
+    result[row, 3:columnCount] <- colMeans(tmp[, 3:columnCount])
+    row <- row + 1
+  }
+}
 
-n <- 3
-y <- matrix(seq(1, n), nrow = n)
-x <- matrix(c(grepthis("-X"), 
-              grepthis("-Y"), 
-              grepthis("-Z")), 
-            ncol = nrow(y))
-dt$featAxis <- factor(x %*% y, 
-                      labels = c(NA, "X", "Y", "Z"))
+# write dataset2
+write.table(result, "tidyset.txt")
 
-r1 <- nrow(dt[, .N, by = c("feature")])
-r2 <- nrow(dt[, .N, by = c("featDomain", "featAcceleration", "featInstrument", "featJerk", "featMagnitude", "featVariable", "featAxis")])
-r1 == r2
-
-###
-# 5. Create a tidy data set
-###
-message("Create a tidy data set...")
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
-
-file <- 'tidy_data.txt'
-write.table(dtTidy, file, quote = F, sep= "\t", row.names = F)
-
-message("ready...")
-
+message("Done writing output...")
